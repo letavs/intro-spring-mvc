6dbe74b96046c44d6d799cf957a9f64fd74e59e4
Leandro Tavares <letavs@gmail.com>
2020-08-03 19:15:57 -0300

    O projeto nesse ponto está danificado por uma exceçao de configuração.

--- src/main/java/br/com/devmedia/curso/config/RootConfig.java
+++ src/main/java/br/com/devmedia/curso/config/RootConfig.java
@@ -1,14 +1,12 @@
 package br.com.devmedia.curso.config;
 
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.ComponentScan.Filter;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.FilterType;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 // Sistema ira buscar toda a configuacao com base nesse pacote
 @Configuration
-@ComponentScan(basePackages = {"br.com.devmedia.curso"}, excludeFilters= {@Filter(type =FilterType.ANNOTATION, value = Configuration.class)})
+@ComponentScan("br.com.devmedia.curso")
 @EnableWebMvc
 public class RootConfig {
 
