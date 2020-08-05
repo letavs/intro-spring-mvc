3b68911aaf7d9ba3a15304c7d1fbb964f01b8997
Leandro Tavares <letavs@gmail.com>
2020-08-04 21:59:07 -0300

    17. Mensagens de validação via arquivos

--- src/main/java/br/com/devmedia/curso/config/SpringMvcConfig.java
+++ src/main/java/br/com/devmedia/curso/config/SpringMvcConfig.java
@@ -1,7 +1,9 @@
 package br.com.devmedia.curso.config;
 
+import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ResourceBundleMessageSource;
 import org.springframework.format.FormatterRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
@@ -25,4 +27,10 @@ public class SpringMvcConfig extends WebMvcConfigurerAdapter {
 	public void addFormatters(FormatterRegistry registry) {
 		registry.addConverter(new TipoSexoConverter());
 	}
+	MessageSource messageSource() {
+		ResourceBundleMessageSource bundleMessageSource =  new ResourceBundleMessageSource();
+		bundleMessageSource.setBasename("messages");
+		return bundleMessageSource;
+	}
+
 }
